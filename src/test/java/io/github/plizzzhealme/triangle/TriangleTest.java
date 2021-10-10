368c6ce8b2c0a529a2d72066f23d09b2c662d4e6
plizzzhealme <plizzz.healme@gmail.com>
2021-10-10 18:43:11 +0300

    add tests

--- src/test/java/io/github/plizzzhealme/triangle/TriangleTest.java
+++ src/test/java/io/github/plizzzhealme/triangle/TriangleTest.java
@@ -9,16 +9,6 @@ public class TriangleTest {
     public static final Double DELTA = 1e-13;
 
     @Test
-    void calcAreaWithValidParameters() {
-        assertEquals(6, Triangle.calcRightTriangleArea(3, 4), DELTA);
-    }
-
-    @Test
-    void calcAreaWithInvalidParameters() {
-        assertThrows(RuntimeException.class, () -> Triangle.calcRightTriangleArea(-1, 4));
-    }
-
-    @Test
     void calcPerimeterWithValidParameters() {
         assertEquals(12, Triangle.calcRightTrianglePerimeter(3, 4), DELTA);
     }
@@ -26,5 +16,15 @@ public class TriangleTest {
     @Test
     void calcPerimeterWithInvalidParameters() {
         assertThrows(RuntimeException.class, () -> Triangle.calcRightTrianglePerimeter(-1, 4));
+    }
+
+    @Test
+    void calcAreaWithValidParameters() {
+        assertEquals(6, Triangle.calcRightTriangleArea(3, 4), DELTA);
+    }
+
+    @Test
+    void calcAreaWithInvalidParameters() {
+        assertThrows(RuntimeException.class, () -> Triangle.calcRightTriangleArea(-1, 4));
     }
 }
